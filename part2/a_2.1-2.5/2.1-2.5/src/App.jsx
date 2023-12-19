21c2d47d5c2403d1476621822e5b33c2279f972d
RanjanKumar93 <rk0936626@gmail.com>
2023-12-19 13:43:04 +0530

    completing part2a exercise 2.4

--- part2/a_2.1-2.5/2.1-2.5/src/App.jsx
+++ part2/a_2.1-2.5/2.1-2.5/src/App.jsx
@@ -1,40 +1,67 @@
-const CourseInfo = ({ courseName, partsName }) => <>
-  <h1>{courseName}</h1>
-  {partsName}
-</>
-
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
-    <> <CourseInfo courseName={course.name} partsName={course.parts.map((x) => <p key={x.id}>{x.name} {x.exercises}</p>)} />
-      <h3>total of {course.parts.reduce((sum, course) => sum + course.exercises, 0)} exercises</h3>
-    </>
+    <div>
+      {course.map((x) => {
+        return (
+          <div key={x.id}>
+            <h1>{x.name}</h1>
+            {x.parts.map((y) => {
+              return (
+                <div key={y.id}>
+                  <p>{y.name} {y.exercises}</p>
+                </div>
+              )
+            })}
+            <h3>total of {x.parts.reduce((sum, a) => sum + a.exercises, 0)} exercises</h3>
+          </div >
+        )
+      })}
+    </div>
   )
 }
 
