878e04cc913a1ceb4fd358151cf3af2c982c91b1
RanjanKumar93 <rk0936626@gmail.com>
2023-12-20 17:24:13 +0530

    completing part2b exercise 2.8

--- part2/b_2.6-2.10/2.6/src/App.jsx
+++ part2/b_2.6-2.10/2.6/src/App.jsx
@@ -4,12 +4,14 @@ const App = () => {
 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('a new name...')
+  const [newNumber, setNewNumber] = useState('')
 
   const submit = (event) => {
     event.preventDefault()
     const newNameObject = {
       name: newName,
-      id: newName
+      id: newName,
+      number: newNumber
     }
 
     for (let i = 0; i < persons.length; i++) {
@@ -19,11 +21,17 @@ const App = () => {
     }
 
     setPersons(persons.concat(newNameObject))
+    setNewNumber('')
     setNewName('')
+    
   }
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -34,11 +42,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
           <button style={{ borderRadius: '5px' }} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((x) => <p style={{ marginBottom: '-15px' }} key={x.id}>{x.name}</p>)}
+      {persons.map((x) => <p style={{ marginBottom: '-15px' }} key={x.id}>{x.name} {x.number}</p>)}
     </div>
   )
 }
