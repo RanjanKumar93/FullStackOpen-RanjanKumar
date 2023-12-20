bb0f8972170ffb73049dea245c252c55d378ce06
RanjanKumar93 <rk0936626@gmail.com>
2023-12-20 20:41:27 +0530

    completing part2b exercises 2.9

--- part2/b_2.6-2.10/2.6/src/App.jsx
+++ part2/b_2.6-2.10/2.6/src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('a new name...')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('search...')
+  const [filteredData, setFilteredData] = useState([])
 
   const submit = (event) => {
     event.preventDefault()
@@ -23,7 +30,6 @@ const App = () => {
     setPersons(persons.concat(newNameObject))
     setNewNumber('')
     setNewName('')
-    
   }
 
   const handleNewName = (event) => {
@@ -34,10 +40,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+
+    const searchPattern = new RegExp(event.target.value, 'i')
+    const filteredResult = persons.filter((y) => searchPattern.test(y.name))
+
+    setFilteredData(filteredResult)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={submit} >
+        <div>
+          filter shown with <input value={search} onChange={handleSearch} />
+        </div>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleNewName} />
         </div>
@@ -49,6 +68,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      <div style={{ marginBottom: '30px' }}>
+        {filteredData.map((x) => <p style={{ marginBottom: '-17px' }} key={x.id}>{x.name} {x.number}</p>)}
+      </div>
       {persons.map((x) => <p style={{ marginBottom: '-15px' }} key={x.id}>{x.name} {x.number}</p>)}
     </div>
   )
