1aed73cbc679d3b1f2667ca80b5d333810c19e67
RanjanKumar93 <rk0936626@gmail.com>
2023-12-28 13:39:11 +0530

    completing ex 2.12 and improving phonebook

---
+++ part2/d_2.12-2.15/2.12/src/App.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Filter = ({ search, handleSearch }) => {
+  return (
+    <form>
+      <div>
+        filter shown with{" "}
+        <input value={search} placeholder="Search..." onChange={handleSearch} />
+      </div>
+    </form>
+  );
+};
+
+const PersonForm = ({
+  submit,
+  newName,
+  handleNewName,
+  newNumber,
+  handleNewNumber,
+}) => {
+  return (
+    <form onSubmit={submit}>
+      <div>
+        name:{" "}
+        <input value={newName} placeholder="name..." onChange={handleNewName} />
+      </div>
+      <div>
+        number:{" "}
+        <input
+          value={newNumber}
+          placeholder="number..."
+          onChange={handleNewNumber}
+        />
+      </div>
+      <div>
+        <button style={{ borderRadius: "5px" }} type="submit">
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ filteredData }) => {
+
+  let show;
+  if(filteredData.length === 0 ){
+    show = <p>nope......</p>
+  }else{
+    show = filteredData.map((x) => (
+      <p style={{marginBottom: '-13px'}} key={x.id}>
+        {x.name} {x.number}
+      </p>
+    ))
+  }
+  return (
+    <>
+      {show}
+    </>
+  );
+};
+
+const App = ({initialData}) => {
+  const [persons, setPersons] = useState([...initialData]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  const submit = (event) => {
+    event.preventDefault();
+    const newNameObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newNameObject.name) {
+        return alert(`${newName} is already added to phonebook`);
+      }
+    }
+
+    axios
+      .post("http://localhost:3001/persons", newNameObject)
+      .then((response) => {
+        setPersons(persons.concat(response.data));
+        setNewName("");
+      });
+  };
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchPattern = new RegExp(search, "i");
+  const filteredData = persons.filter((y) => searchPattern.test(y.name));
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter search={search} handleSearch={handleSearch} />
+      <h2>add a new</h2>
+      <PersonForm
+        submit={submit}
+        newName={newName}
+        handleNewName={handleNewName}
+        handleNewNumber={handleNewNumber}
+        newNumber={newNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons filteredData={filteredData} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/d_2.12-2.15/2.12/src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import axios from 'axios'
+import App from './App'
+
+axios.get('http://localhost:3001/persons').then(response => {
+  const initialData = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App initialData={initialData} />)
+})
